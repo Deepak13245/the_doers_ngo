71027d6377ef8fb5da90b7e9c226fbce4794b6dd
Deepak Kumar T P <deepak@dbuddy.in>
2018-02-16 23:58:14 +0530

    Final push

--- resources/views/auth.blade.php
+++ resources/views/auth.blade.php
@@ -44,7 +44,7 @@
                     @endif
                     @if(session()->has('message'))
                         <div class="alert alert-{{ session()->get('type') }}">
-
+                            {{ session()->get('message') }}
                         </div>
                     @endif
                     <div class="row">
